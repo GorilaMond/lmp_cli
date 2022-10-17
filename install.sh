ac72dbcc5ed417d38ad9dbc517943eb6bc78fe08
LiuLingze <luiyanbing@foxmail.com>
2022-10-17 11:51:08 +0800

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 if [ -n "$(uname -i | grep x86)" ]; then
 	wget https://aka.pw/bpf-ecli -O ecli && chmod +x ecli && sudo mv ecil /usr/bin
+	wget https://github.com/GorilaMond/lmp_cli/releases/download/lmp/lmp_x86 -O lmp && chmod +x lmp && sudo mv lmp /usr/bin
 else
 	echo Only support x86 plateform now.
 fi
